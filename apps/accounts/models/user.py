7e9bcdc13a7573beb6b0e68b774b5ba99d35f9c2
genesisblock3301 <nur15-1463@diu.edu.bd>
2024-09-21 19:42:51 +0600

    refactoring authentication code

--- apps/accounts/models/user.py
+++ apps/accounts/models/user.py
@@ -3,6 +3,7 @@ from datetime import timedelta, timezone
 import jwt
 from django.db import models, transaction
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from setuptools.extern import names
 
 from apps.utilities.models import TimeStamp
 
@@ -45,7 +46,7 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.is_staff = True
         user.is_active = True
-        user.user_type = 1
+        user.user_type = UserRole.objects.filter(name='admin').first()
         user.set_password(password)
         user.save()
         return user
