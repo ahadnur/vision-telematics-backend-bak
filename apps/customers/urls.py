a728aa280ae1655f5b26321067faaa7ad15035bc
genesisblock3301 <nur15-1463@diu.edu.bd>
2024-10-11 00:24:48 +0600

    refactoring account url and view code

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -6,9 +6,9 @@ app_name = 'customers'
 
 urlpatterns = [
     path('company-list/', CustomerCompanyListAPIView.as_view(), name='company-list'),
-    path('customer-list/', CustomerListAPIView.as_view(), name='customer-list'),
-    path('customer-create/', CustomerCreateAPIView.as_view(), name='customer-create'),
-    path('customer-update/<pk>/', CustomerUpdateAPIView.as_view(), name='customer-update'),
+    path('list/', CustomerListAPIView.as_view(), name='customer-list'),
+    path('create/', CustomerCreateAPIView.as_view(), name='customer-create'),
+    path('update/<pk>/', CustomerUpdateAPIView.as_view(), name='customer-update'),
     path('create-vehicle-info/', CreateCustomerVehicleAPIView.as_view(), name='customer-vehicle-create'),
     path('get-vehicle-info/', GetVehicleInfoAPIView.as_view(), name='customer-get-vehicle-info'),
 ]
