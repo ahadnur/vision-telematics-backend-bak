32bba84d0268f836064327477bc9b88eef232bae
genesisblock3301 <nur15-1463@diu.edu.bd>
2024-11-23 16:38:40 +0600

    refactoring customer vehicle info

--- apps/customers/views/customer_vehicle_info.py
+++ apps/customers/views/customer_vehicle_info.py
@@ -1,4 +1,4 @@
-from rest_framework import status, views
+from rest_framework import status
 from rest_framework.response import Response
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
