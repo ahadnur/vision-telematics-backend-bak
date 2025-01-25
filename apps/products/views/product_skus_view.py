c272dbe318f4e78084c57352718a423dee7e8794
genesisblock3301 <nur15-1463@diu.edu.bd>
2025-01-25 17:06:05 +0600

    product paginated less list and get product skus with product_id.

--- apps/products/views/product_skus_view.py
+++ apps/products/views/product_skus_view.py
@@ -1,7 +1,7 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.generics import ListAPIView
 
 from apps.products.models import ProductSKU
 from apps.products.serializers import GetProductSKUByProductIDSerializer
